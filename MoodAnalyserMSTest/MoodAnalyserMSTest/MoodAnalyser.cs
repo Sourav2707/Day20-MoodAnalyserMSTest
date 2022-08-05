d4d25971e5d5bbbeaaf2ad291c4687139b732a96
Sourav2707 <smartsourav33@gmail.com>
2022-08-05 14:29:49 +0530

    Analysing the user mood from the given message

---
+++ MoodAnalyserMSTest/MoodAnalyserMSTest/MoodAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserMSTest
+{
+    public class MoodAnalyser
+    {
+        private string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public void analyseMood()
+        {
+            if (this.message.ToUpper().Contains("SAD"))
+                Console.WriteLine("The user mood is SAD");
+            else
+                Console.WriteLine("The user mood is HAPPY");
+        }
+    }
+}
--- MoodAnalyserMSTest/MoodAnalyserMSTest/Program.cs
+++ MoodAnalyserMSTest/MoodAnalyserMSTest/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Welcome to MoodAnalyser MSTesting");
+﻿using MoodAnalyserMSTest;
+
+Console.WriteLine("Welcome to MoodAnalyser MSTesting");
+Console.WriteLine("Enter your mood to check");
+string mood = Console.ReadLine();
+MoodAnalyser obj = new MoodAnalyser(mood);
+obj.analyseMood();
