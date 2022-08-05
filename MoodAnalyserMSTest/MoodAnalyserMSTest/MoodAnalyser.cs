eb45007d30333d95ab13dfabf7e400a94508f10b
Sourav2707 <smartsourav33@gmail.com>
2022-08-05 16:28:29 +0530

    Test case 1 with exception handling

--- MoodAnalyserMSTest/MoodAnalyserMSTest/MoodAnalyser.cs
+++ MoodAnalyserMSTest/MoodAnalyserMSTest/MoodAnalyser.cs
@@ -18,10 +18,31 @@ namespace MoodAnalyserMSTest
         }
         public string analyseMood()
         {
-            if (this.message.ToUpper().Contains("SAD"))
-                return "SAD";
-            else
-                return "HAPPY";
+            try
+            {
+                if (this.message.ToUpper().Contains("SAD"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+            }
+            catch (NullReferenceException obj)
+            {
+
+                throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+            catch (MoodAnalyserException ex)
+            {
+                Console.WriteLine("this is our custom exception");
+            }
+            finally
+            {
+                Console.WriteLine("This code will always execute irrespective of exception came or not!!!!");
+            }
+            return null;
         }
     }
 }
