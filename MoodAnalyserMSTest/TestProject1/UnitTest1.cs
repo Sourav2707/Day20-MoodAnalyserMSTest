036c6d5d6c4d246257e55e4a092e8ecd42d0fa3c
Sourav2707 <smartsourav33@gmail.com>
2022-08-11 13:34:38 +0530

    Test case with empty exception

--- MoodAnalyserMSTest/TestProject1/UnitTest1.cs
+++ MoodAnalyserMSTest/TestProject1/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace TestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            MoodAnalyser obj = new MoodAnalyser(null);
+            MoodAnalyser obj = new MoodAnalyser("");
             string result = obj.analyseMood();
             Assert.AreEqual(null, result);
         }
