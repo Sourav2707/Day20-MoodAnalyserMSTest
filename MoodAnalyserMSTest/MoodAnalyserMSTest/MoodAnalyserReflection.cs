de6b8b7d806632ab8b5ecabbe6c4346234576563
Sourav2707 <smartsourav33@gmail.com>
2022-08-25 11:50:37 +0530

    MoodAnalyser using Reflection with exception

--- MoodAnalyserMSTest/MoodAnalyserMSTest/MoodAnalyserReflection.cs
+++ MoodAnalyserMSTest/MoodAnalyserMSTest/MoodAnalyserReflection.cs
@@ -18,11 +18,26 @@ namespace MoodAnalyserMSTest
             string pattern = @"." + constructorName + "$";
             Match result = Regex.Match(className, pattern);
             if (result.Success)
+                try
                 {
                     Assembly executing = Assembly.GetExecutingAssembly();
                     Type moodAnalyseType = executing.GetType(className);
                     return Activator.CreateInstance(moodAnalyseType);
                 }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Your input is not valid");
+                    throw new CustomException(CustomException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something wrong happened.");
+                }
+            else
+            {
+                throw new CustomException(CustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is not found");
+            }
             return null;
         }
     }
